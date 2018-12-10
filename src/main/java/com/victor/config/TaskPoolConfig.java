54332c686a00d7d12604656c616ce8ea5d4231ed
wangqianyuan123 <30424785+wangqianyuan123@users.noreply.github.com>
2018-12-10 14:56:50 +0800

    优雅的关闭线程池

--- src/main/java/com/victor/config/TaskPoolConfig.java
+++ src/main/java/com/victor/config/TaskPoolConfig.java
@@ -17,7 +17,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
  * 线程池对拒绝任务的处理策略：这里采用了CallerRunsPolicy策略，当线程池没有处理能力的时候，该策略会直接在 execute 方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
  * @author victor
  *
- */
+*/
 @Configuration
 public class TaskPoolConfig {
 
