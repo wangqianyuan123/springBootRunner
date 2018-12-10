f58cacfa80c3c7c5e6aa68e60e1fc0e21c75d20c
wangqianyuan123 <30424785+wangqianyuan123@users.noreply.github.com>
2018-12-10 15:05:48 +0800

    代码修改

--- src/main/java/com/victor/controller/errController.java
+++ src/main/java/com/victor/controller/errController.java
@@ -1,5 +1,8 @@
 package com.victor.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,10 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 public class errController {
 
+	  @Resource
+	    RedisTemplate<String, Object> redisTemplate;
+	  
+	  
 	/**
 	 * 1.默认的异常处理 界面显示（http://localhost:8080/hello）
 	 * Whitelabel Error Page
@@ -30,8 +37,12 @@ public class errController {
 	@ApiOperation(value="返回json", notes="json格式的异常统一处理")
     @ApiImplicitParam(name = "json", value = "无需输入参数", required = true, dataType = "demo1")
 	@RequestMapping("data/hello")
-	public String hello1() throws Exception {
-	    throw new MyDataException("发生错误");
+	public String hello1(String datakey) throws Exception {
+//	    throw new MyDataException("发生错误");
+		//RELATION_M1168
+		System.out.println(redisTemplate.delete(datakey));
+		return "123";
+
 	}
 	
 	/**
--- src/main/java/com/victor/dao/master/ApiMasterDao.java
+++ src/main/java/com/victor/dao/master/ApiMasterDao.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.victor.model.UserDto;
 
 
 
+@Mapper
 public interface ApiMasterDao {
 
 	// 获取 key值对应sql
@@ -29,4 +31,12 @@ public interface ApiMasterDao {
 	 */
 	@Insert("insert into userInfo(ID,USER_NAME,BIRTHDAY) values(#{id},#{userName},#{birthday})")
 	public int insertUser(UserDto userDto);
+	
+	/**
+	 * 注解形式
+	 * @param userDto
+	 * @return
+	 */
+	@Insert("update userInfo(ID,USER_NAME,BIRTHDAY) values(#{id},#{userName},#{birthday})")
+	public int updateUser(UserDto userDto);
 }
